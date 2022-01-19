63417b240580e28ee85234d61df179e7ab339f97
Alex Zissis <alex@zico.dev>
2022-01-19 21:21:10 +1100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    plugins: ['@typescript-eslint', 'prettier', 'import'],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+
+    rules: {
+        '@typescript-eslint/no-unused-vars': [
+            2,
+            {varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_'},
+        ],
+        '@typescript-eslint/no-explicit-any': 0,
+        '@typescript-eslint/no-namespace': 0,
+        'import/extensions': [2, 'ignorePackages'],
+    },
+
+    env: {
+        browser: true,
+        es2021: true,
+    },
+};
+
